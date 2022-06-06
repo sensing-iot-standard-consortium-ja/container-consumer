9ed1018c843cb6683f63209d506951eea398aaff
seiketkm <seike.takumi.999@tis.co.jp>
2022-06-06 14:08:52 +0900

    wip kafka

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module protoschema
 go 1.18
 
 replace (
+	protoschema/Common => ./Common
 	protoschema/Container => ./Container
 	protoschema/Schema => ./Schema
-	protoschema/Common => ./Common
 	protoschema/Sensors/MPU6050 => ./Sensors/MPU6050
 	protoschema/Sensors/MPU6050Arrays => ./Sensors/MPU6050Arrays
 )
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
--- main_test.go
+++ main_test.go
@@ -10,6 +10,8 @@ import (
 	"protoschema/Container"
 	"protoschema/Schema"
 	"testing"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func TestCreateData(t *testing.T) {
@@ -44,7 +46,6 @@ func TestPayloadParse(t *testing.T) {
 	container := Container.Marshal(container_bin)
 
 	dataIdBin, _ := hex.DecodeString("00112233445566778899aabbccddeeff")
-
 	if !bytes.Equal(container.Header.DataId, dataIdBin) {
 		t.Errorf("DataID is 0x00112233445566778899aabbccddeeff")
 	}
@@ -60,10 +61,63 @@ func TestPayloadParse(t *testing.T) {
 		fmt.Println(ss.Value)
 		fmt.Println(ss.Payload)
 	}
-
+	fmt.Printf("%X\n", container.Payload)
 	// goroutin
 	// for field := range s.Fields {
 	// 	field.Unmarshal()
 	// }
+
+}
+
+func TestContainerToKafkaProduce(t *testing.T) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					// parse dataId
+					container := Container.Marshal(ev.Value)
+					dataId, _ := hex.DecodeString("00112233445566778899aabbccddeeff")
+					var dataIdx uint8 = 0
+					if bytes.Equal(container.Header.DataId, dataId) && container.Header.DataIndex == dataIdx {
+						f, _ := os.Open("tests_examples/0_00112233445566778899aabbccddeeff.json")
+						// Schemaの読み込み
+						schema, _ := ioutil.ReadAll(f)
+						s := Schema.Schema{}
+						json.Unmarshal([]byte(schema), &s)
+						stPayload, _ := s.Marshal(container.Payload)
+						for _, ss := range stPayload {
+							fmt.Println(ss.Name)
+							fmt.Println(ss.Value)
+							fmt.Println(ss.Payload)
+						}
+					}
+				}
+			}
+		}
+	}()
+
+	container_hexascii := "000000490000112233445566778899aabbccddeeff1d17bcbbbfda7efb68a82a40c019f1c5a92eed6440838c6cab4ec4407668fd38adaade4047752df44eb70c02296b8194cbf0cbf0"
+	container_bin, _ := hex.DecodeString(container_hexascii)
+
+	// Produce messages to topic (asynchronously)
+	topic := "myTopic"
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          container_bin,
+	}, nil)
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
 
 }
